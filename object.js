508f15b82ba12bdc05f64784c3d5f5f1e12acbac
pooja <pooja@>
2022-11-01 17:44:21 +0530

    js object

---
+++ object.js
@@ -0,0 +1,12 @@
+let student = {
+    firstName: "Ananya",
+    lastName:"Ambi",
+    age: 2,
+    marks:78
+
+}
+
+for(let key in student){
+    console.log(key);
+    console.log(student[key]);
+}
